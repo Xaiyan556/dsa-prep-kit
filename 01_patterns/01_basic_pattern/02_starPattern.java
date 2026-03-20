96d959f11895f96ef61d39d781e8c614fb3fee1b
Xaiyan@556 <ayansharmasde@gmail.com>
2026-03-20 12:05:54 +0530

    star pattern

---
+++ 01_patterns/01_basic_pattern/02_starPattern.java
@@ -0,0 +1,15 @@
+public class StarPattern {
+    public static void main(String[] args) {
+
+        int n = 4;   // number of rows
+
+        for(int i = 1; i <= n; i++) {      // outer loop → rows
+            
+            for(int j = 1; j <= i; j++) {  // inner loop → stars
+                System.out.print("* ");
+            }
+
+            System.out.println(); // move to next line
+        }
+    }
+}
